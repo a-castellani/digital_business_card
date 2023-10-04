ab34cf151176034f95bb7f696d21df0d0b2cd290
Andres Castellani <andresadc@gmail.com>
2023-10-04 23:11:25 +0200

    first react challenge

---
+++ components/Info.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <div>
+            <div className="info-text">
+                <h2>Andres Castellani</h2>
+                <h4>Frontend developer</h4>
+                <a href="https://a-castellani.github.io">Website</a>
+            </div>
+            <div className="displaybtn">
+                <a href="https://github.com/a-castellani" className="btngithub">Github</a>
+                <a href="https://linkedin.com/in/a-castellani" className="btnlinkedin">Linkedin</a>
+            </div>
+        </div>
+    )
+}
